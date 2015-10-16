50b7fb894b8744ad8f03904e221978887b78c634
saifcoding <saif.as7777@gmail.com>
2015-10-16 16:07:04 +0300

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,31 @@
+/*
+ * Game.h
+ *
+ *  Created on: Oct 11, 2015
+ *      Author: Admin
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+using namespace std;
+#include <iostream>
+#include <cstring>
+class Game {
+	string player1;
+	string player2;
+	enum {SIZE=3} ;
+	char table[SIZE][SIZE];
+public:
+	Game();
+	void findX();
+	void setPlayer1(string );
+	void setPlayer2(string );
+	string getPlayer1() const;
+	string getPlayer2() const;
+	bool checkifDone() const;
+	int playIn(int,int,int);
+	void printTable()const;
+	int checkWin() const;
+};
+
+#endif /* GAME_H_ */
